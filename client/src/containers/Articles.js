f40e52437768ef00384ed3405aaac44ff5ce1f08
Romantic <595159090004@dpu.ac.th>
2018-02-03 15:52:20 +0700

    React Router v4: Location

--- client/src/containers/Articles.js
+++ client/src/containers/Articles.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 
 class ArticlesContainer extends Component {
   state = {
-    articles: []
+    articles: [],
+    pagination: {}
   }
 
   // Lifecycle method
@@ -13,39 +15,75 @@ class ArticlesContainer extends Component {
 
   // Lifecycle method
   componentDidUpdate(prevProps) {
-    const { match } = this.props
+    const { match, location } = this.props
     // : use for change name match to prevMatch
-    const { match: prevMatch } = prevProps
+    const { match: prevMatch, location: prevLocation } = prevProps
+    // call Location from Route props
+    /*
+    console.log(this.props.location)
+    console.log(new URLSearchParams(this.props.location.search).get('page'))
+    */
 
-    if( match.params.categoryId !== prevMatch.params.categoryId ) {
+    if( 
+        (match.params.categoryId !== prevMatch.params.categoryId) ||
+        (location.search !== prevLocation.search)
+      ) {
       this.loadArticles()
     }
   }
 
   loadArticles() {
-    const { categoryId } = this.props.match.params
+    const { location: { search }, match: { params }} = this.props
+    const { categoryId } = params
+    const page = new URLSearchParams(search).get('page')
 
-    fetch(`/articles?categoryId=${categoryId}`)
+    fetch(`/articles?categoryId=${categoryId}&page=${page || 1}`)
       .then(res => res.json())
-      .then(({ articles }) => this.setState({ articles }))
+      //.then(json => console.log(json))
+      .then(({ articles, meta }) => this.setState({ articles, pagination: meta }))
   }
 
   render() {
-    const { articles } = this.state
+    const { articles, pagination: { page, totalPages } } = this.state
+    const { categoryId } = this.props.match.params
 
     return(
-      <ul className='nav flex-column'>
-        {
-          articles.map(({id, title}) => 
-            <Link
-              key={id}
-              to={`/articles/${id}`}
-              className='nav-link'>
-                {title}
-            </Link>
-          )
-        }
-      </ul>
+      <div>
+        <ul className='nav flex-column'>
+          {
+            articles.map(({id, title}) => 
+              <Link
+                key={id}
+                to={`/articles/${id}`}
+                className='nav-link'>
+                  {title}
+              </Link>
+            )
+          }
+        </ul>
+        <nav>
+          <ul className='pagination'>
+            {
+              Array.from({ length: totalPages }).map((_, index) => {
+                const currentIndex = index + 1
+
+                return (
+                  <li 
+                    key={currentIndex}
+                    className={classNames(['page-item', { active: currentIndex === +page }])}>
+                      <Link
+                        to={`/categories/${categoryId}/articles?page=${currentIndex}`}
+                        className='page-link'>
+                          {currentIndex}
+                      </Link>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </nav>
+      </div>
+
     )
   }
 }
