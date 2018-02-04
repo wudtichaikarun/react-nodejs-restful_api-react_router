f502b39577ff638213675723d5ed957f5162ea5c
Romantic <595159090004@dpu.ac.th>
2018-02-04 16:06:04 +0700

    React Router v4: Not Found

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import { Home, Categories, Articles } from './'
+import { Route, Switch } from 'react-router-dom'
+import { Home, Categories, Articles, NotFound } from './'
 import { Sidebar } from '../containers'
 
 const Content = () => (
   <div className='row'>
     <Sidebar />
     <div className='col-7'>
+    <Switch>
       <Route exact path='/' component={Home} />
       <Route path='/categories' component={Categories} />
       <Route path='/articles' component={Articles} />
+      <Route component={NotFound}/>
+    </Switch>
     </div>
   </div> 
 )
