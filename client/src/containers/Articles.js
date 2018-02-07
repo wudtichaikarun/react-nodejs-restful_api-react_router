073cc5a7e0bd87ddae41e8a2d9236af76d49eef9
Romantic <595159090004@dpu.ac.th>
2018-02-07 22:15:19 +0700

    Recompose: Articles use stateless fnc component (compose)

--- client/src/containers/Articles.js
+++ client/src/containers/Articles.js
@@ -1,12 +1,71 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { numericString } from 'airbnb-prop-types'
 import { Link } from 'react-router-dom'
 import classNames from 'classnames'
+import {
+  setPropTypes,
+  withProps,
+  withState,
+  withHandlers,
+  lifecycle,
+  compose
+} from 'recompose'
 import Auth from '../lib/Auth';
 
-class ArticlesContainer extends Component {
-  static propTypes = {
+const ArticlesContainer = ({
+  categoryId,
+  articles,
+  pagination: { totalPages, page }
+}) => (
+  <div>
+    {
+      Auth.getToken() && (
+        <Link 
+          to={`/categories/${categoryId}/articles/new`}
+          className='btn btn-sm btn-secondary'>
+            Create Article
+        </Link>
+      )
+    }
+    <ul className='nav flex-column'>
+      {
+        articles.map(({id, title}) => 
+          <Link
+            key={id}
+            to={`/articles/${id}`}
+            className='nav-link'>
+              {title}
+          </Link>
+        )
+      }
+    </ul>
+    <nav>
+      <ul className='pagination'>
+        {
+          Array.from({ length: totalPages }).map((_, index) => {
+            const currentIndex = index + 1
+
+            return (
+              <li 
+                key={currentIndex}
+                className={classNames(['page-item', { active: currentIndex === +page }])}>
+                  <Link
+                    to={`/categories/${categoryId}/articles?page=${currentIndex}`}
+                    className='page-link'>
+                      {currentIndex}
+                  </Link>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </nav>
+  </div>
+)
+
+export default compose(
+  setPropTypes({
     match: PropTypes.shape({
       params: PropTypes.shape({
         categoryId: numericString().isRequired
@@ -15,101 +74,47 @@ class ArticlesContainer extends Component {
     location: PropTypes.shape({
       search: PropTypes.string.isRequired
     }).isRequired
+  }),
 
-  }
+  withState('articles', 'setArticles', []),
+  withState('pagination', 'setPagination', {}),
 
-  state = {
-    articles: [],
-    pagination: {}
-  }
+  withProps(props => ({ categoryId: props.match.params.categoryId })),
 
-  // Lifecycle method
-  componentDidMount() {
-    this.loadArticles()
-  }
+  withHandlers({
+    loadArticles: ({
+      setArticles,
+      setPagination, 
+      location: { search }, 
+      categoryId
+      }) => _ => {
+        const page = new URLSearchParams(search).get('page')
 
-  // Lifecycle method
-  componentDidUpdate(prevProps) {
-    const { match, location } = this.props
-    // : use for change name match to prevMatch
-    const { match: prevMatch, location: prevLocation } = prevProps
-    // call Location from Route props
-    /*
-    console.log(this.props.location)
-    console.log(new URLSearchParams(this.props.location.search).get('page'))
-    */
+        fetch(`/articles?categoryId=${categoryId}&page=${page || 1}`)
+          .then(res => res.json())
+          //.then(json => console.log(json))
+          .then(({ articles, meta }) => {
+            setArticles(articles)
+            setPagination(meta)
+          })
+      } 
+  }),
 
-    if( 
-        (match.params.categoryId !== prevMatch.params.categoryId) ||
-        (location.search !== prevLocation.search)
-      ) {
-      this.loadArticles()
+  lifecycle({
+    componentDidMount() {
+      this.props.loadArticles()
+    },
+
+    componentDidUpdate(prevProps) {
+      const { match, location } = this.props
+      const { match: prevMatch, location: prevLocation } = prevProps
+      if( 
+          (match.params.categoryId !== prevMatch.params.categoryId) ||
+          (location.search !== prevLocation.search)
+        ) {
+        this.props.loadArticles()
+      }
     }
-  }
+  })
 
-  loadArticles() {
-    const { location: { search }, match: { params }} = this.props
-    const { categoryId } = params
-    const page = new URLSearchParams(search).get('page')
-
-    fetch(`/articles?categoryId=${categoryId}&page=${page || 1}`)
-      .then(res => res.json())
-      //.then(json => console.log(json))
-      .then(({ articles, meta }) => this.setState({ articles, pagination: meta }))
-  }
-
-  render() {
-    const { articles, pagination: { page, totalPages } } = this.state
-    const { categoryId } = this.props.match.params
-
-    return(
-      <div>
-        {
-          Auth.getToken() && (
-            <Link 
-              to={`/categories/${categoryId}/articles/new`}
-              className='btn btn-sm btn-secondary'>
-                Create Article
-            </Link>
-          )
-        }
-        <ul className='nav flex-column'>
-          {
-            articles.map(({id, title}) => 
-              <Link
-                key={id}
-                to={`/articles/${id}`}
-                className='nav-link'>
-                  {title}
-              </Link>
-            )
-          }
-        </ul>
-        <nav>
-          <ul className='pagination'>
-            {
-              Array.from({ length: totalPages }).map((_, index) => {
-                const currentIndex = index + 1
-
-                return (
-                  <li 
-                    key={currentIndex}
-                    className={classNames(['page-item', { active: currentIndex === +page }])}>
-                      <Link
-                        to={`/categories/${categoryId}/articles?page=${currentIndex}`}
-                        className='page-link'>
-                          {currentIndex}
-                      </Link>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </nav>
-      </div>
-
-    )
-  }
-}
-
-export default ArticlesContainer
+)(ArticlesContainer)
