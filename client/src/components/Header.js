ae5f4e8620a8b39743a9ccdde997e5119f5336d9
Romantic <595159090004@dpu.ac.th>
2018-02-08 15:39:27 +0700

    Recompose: brance error

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, withRouter } from 'react-router-dom'
+import {
+  setPropTypes,
+  withHandlers,
+  branch,
+  compose
+} from 'recompose'
 import { Auth } from '../lib'
 
-const logout = (push) => {
-  return () => {
-    Auth.removeToken()
-    push('/')
-  }  
-}
+const Nav = ({ children }) => (
+  <nav className='navbar navbar-toggleable-md navbar-light bg-faded md-3'>
+    { children }
+  </nav> 
+)
 
-const Header = ({ history: { push }}) => {
-  const links = Auth.getToken() ? (
+const UserManu = ({logout}) => (
+  <Nav>
     <ul className='navbar-nav ml-auto'>
       <li className='nav-item'>
-        <a href='#' className='nav-link' onClick={logout(push)}>Logout</a>
+        <a href='#' className='nav-link' onClick={logout}>Logout</a>
       </li>
     </ul>
-  ) : (
+  </Nav>
+
+)
+
+const GuestManu = () => (
+  <Nav>
     <ul className='navbar-nav ml-auto'>
       <li className='nav-item'>
         <Link to='/sign-in' className='nav-link'>Login</Link>
@@ -26,19 +36,33 @@ const Header = ({ history: { push }}) => {
         <Link to='/sign-up' className='nav-link'>Register</Link>
       </li>
     </ul>
+  </Nav>
+)
+
+export default compose(
+  withRouter,
+
+  setPropTypes({
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired
+    }).isRequired
+  }),
+
+  withHandlers({
+    logout: ({
+      history: { push }
+    }) => _ => {
+      return () => {
+        Auth.removeToken()
+        push('/')
+      }
+    }
+  }),
+
+  branch(
+    _ => Auth.getToken(),
+    UserManu,
+    GuestManu 
   )
 
-  return (
-    <nav className='navbar navbar-toggleable-md navbar-light bg-faded md-3'>
-      { links }
-    </nav> 
-  )
-}
-
-Header.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired
-  }).isRequired
-}
-
-export default withRouter(Header)
+)()
