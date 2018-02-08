cea86551580ebe04c60f79302d7c10bc437e17bb
Romantic <595159090004@dpu.ac.th>
2018-02-08 14:38:25 +0700

    Recompose: make Signin component to be stateless functional component

--- client/src/containers/Signin.js
+++ client/src/containers/Signin.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {
+ setPropTypes,
+ withHandlers,
+ compose 
+} from 'recompose'
 import { Auth } from '../lib'
 import { AuthForm } from '../components'
 
-class SigninContainer extends Component {
-  handleFormSubmit = credential => {
-    fetch('/sessions', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(credential)
-    })
-      .then(({headers}) => Auth.setToken(headers))
-      .then(() => this.props.history.goBack())
-  }
-  render() {
-    return(
-      <AuthForm
-        formName='Signin'
-        onSubmit={this.handleFormSubmit} />
-    )
-  }
-}
+const SigninContainer = ({
+  onSubmit
+}) => (
+  <AuthForm
+  formName='Signin'
+  onSubmit={onSubmit}/>
+)
 
-export default SigninContainer
+export default compose(
+  setPropTypes({
+    history: PropTypes.shape({
+      goBack: PropTypes.func.isRequired
+    }).isRequired
+  }),
+
+  withHandlers({
+    onSubmit: ({ history: { goBack } }) => credential => {
+      fetch('/sessions', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credential)
+      })
+        .then(({headers}) => Auth.setToken(headers))
+        .then(() => goBack())
+    }
+  })
+)(SigninContainer)
